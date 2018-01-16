69ed24e9375f2442fb7b28d64b458a48847c4c3c
patriksuska <patriksuska11@gmail.com>
2018-01-16 17:54:25 +0100

    primer intento de programa funcionando

---
+++ Acepta_el_Reto/src/acepta_el_reto/Tendencia_lado_oscuro.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acepta_el_reto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author patsus
+ */
+public class Tendencia_lado_oscuro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Cuantos casos de prueba
+        Scanner sc=new Scanner (System.in);
+        int cant=sc.nextInt();
+        int num;
+        int y;
+        int j;
+        int cont=0;
+        // Guardamos todos los casos
+        int v[]=new int[cant];
+        for (int i=0;i<cant;i++)
+        {
+            num=sc.nextInt();
+            v[i]=num;
+        }
+        
+        // Para cada caso
+        for (j=0;j<cant;j++){
+            char[] resultado=Integer.toString(v[j],5).toCharArray();
+            for(y=0;y<resultado.length;y++){
+            
+                if(resultado[y]=='4')
+                    {
+                        cont++;
+                    }
+            }
+            if (cont>=2)
+            {
+                System.out.println("SI");
+            }
+            else
+                System.out.println("NO");
+        }
+    }
+    
+}
