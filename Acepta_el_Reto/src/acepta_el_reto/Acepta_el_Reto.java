4df8ed84beb478aab809dcb145c7818ea2733299
patriksuska <patriksuska11@gmail.com>
2018-01-16 16:46:58 +0100

    añadido .java

--- Acepta_el_Reto/src/acepta_el_reto/Acepta_el_Reto.java
+++ Acepta_el_Reto/src/acepta_el_reto/Acepta_el_Reto.java
@@ -17,6 +17,7 @@ public class Acepta_el_Reto {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
+        System.out.println("Hola");
     }
     
 }
