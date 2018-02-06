58cea4c4ac1c57d2a91754f8048fd16cc8f916a6
patriksuska <patriksuska11@gmail.com>
2018-02-06 18:59:53 +0100

    creado funcionando al 50% invertir_jaen.java

--- Acepta_el_Reto/src/acepta_el_reto/invertir_jaen.java
+++ Acepta_el_Reto/src/acepta_el_reto/invertir_jaen.java
@@ -13,8 +13,6 @@ import java.util.Scanner;
  */
 
 public class invertir_jaen {
-    public static int cont=0;
-    public static int espai=0;
     public static int contar(char [][] matriz,int i,int j){
        int filas=matriz.length;
        int columnas=matriz[0].length;
@@ -23,19 +21,23 @@ public class invertir_jaen {
        {
            return 0;
        }
+       if(matriz[i][j]==' ')
+       {
+           return 0;
+       }
        if (matriz[i][j]=='#')
        {
-           return 1+contar(matriz,i,j+1);
+           return 1+(contar(matriz,i,j+1)+contar(matriz,i,j-1)+contar(matriz,i+1,j)+contar(matriz,i-1,j));
        }
        else//comparar cual es mayor y cuando termina puesto en space,guarda el numero total en una posicion del vector.
-        return 0+contar(matriz,i,j+1);
-       
+        return 0;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x0;
-        int y0;
-        int resultado;
+        
+        int x;
+        int y;
+        int resultado=0;
         do {
             //procesar filas
             int filas = sc.nextInt();
@@ -44,15 +46,20 @@ public class invertir_jaen {
             sc.nextLine();
             //creo las filas y columnas
             char[][] matriz = new char[filas][colum];
-            for (int x=0;x<filas;x++){
+            for (x=0;x<filas;x++){
                 //pido datos
                 String linea = sc.nextLine();
-                for(int y=0;y<colum;y++){
+                for(y=0;y<colum;y++){
                     matriz[x][y]=linea.charAt(y);
                     //compruebo que este la #
+                    if(matriz[x][y]=='#')
+                    {
+                        int a=x;
+                        int b=y;
+                        resultado+=contar(matriz,a,b);
+                    }
                 }
             }
-            resultado=contar(matriz,0,0);
             System.out.println(resultado);
         }while(sc.hasNext());
         sc.close();
